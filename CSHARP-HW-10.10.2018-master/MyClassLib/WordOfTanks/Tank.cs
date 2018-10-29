09f6d4a216130c21afa62888f4a631dffa4f26bb
TazhibekA <40587394+TazhibekA@users.noreply.github.com>
2018-10-29 22:01:05 +0600

    Add files via upload

---
+++ CSHARP-HW-10.10.2018-master/MyClassLib/WordOfTanks/Tank.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyClassLib.WordOfTanks
+{
+    public class Tank
+    {
+        public string Name { get; set; }
+        public int AmmunitionLevel { get; set; }
+        public int ArmorLevel { get; set; }
+        public int ManeuverabilityLevel { get; set; }
+        static Random rnd = new Random();
+
+        public Tank(string name)
+        {
+            this.Name = name;
+            this.AmmunitionLevel = rnd.Next(0, 100);
+            this.ArmorLevel = rnd.Next(0, 100);
+            this.ManeuverabilityLevel = rnd.Next(0, 100);
+
+        }
+
+        public void GetInfo() {
+            Console.WriteLine("Ammunition Level" + AmmunitionLevel);
+            Console.WriteLine("Armor Level" + ArmorLevel);
+            Console.WriteLine("Maneuverability Level" + ManeuverabilityLevel);
+        }
+    }
+}
---
+++ CSHARP-HW-10.10.2018-master/MyClassLib/WordOfTanks/Team.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyClassLib.WordOfTanks
+{
+    public class Team
+    {
+
+        public string TeamName { get; set; }
+        public List<Tank> Tanks { get; set; }
+
+        public Team(string teamName, List<Tank> tanks)
+        {
+            Tanks = new List<Tank>();
+            TeamName = teamName;
+            Tanks = tanks;
+        }
+
+
+        public void  AddTank(Tank tank) {
+            Tanks.Add(tank);
+        }
+ 
+
+        public static  Team operator *(Team firstTeam, Team secondTeam)
+        {
+            int firstTeamPoints = 0;
+            int secondTeamPoints = 0;
+
+            for (int i = 0; i < 5; i++)
+            {
+                if (firstTeam.Tanks[i].AmmunitionLevel > secondTeam.Tanks[i].AmmunitionLevel)
+                    firstTeamPoints++;
+                else
+                    secondTeamPoints++;
+
+                if (firstTeam.Tanks[i].ArmorLevel > secondTeam.Tanks[i].ArmorLevel)
+                    firstTeamPoints++;
+                else
+                    secondTeamPoints++;
+
+                if (firstTeam.Tanks[i].ManeuverabilityLevel > secondTeam.Tanks[i].ManeuverabilityLevel)
+                    firstTeamPoints++;
+                else
+                    secondTeamPoints++;
+            }
+
+          
+
+            if (firstTeamPoints > secondTeamPoints)
+                return firstTeam;
+            else
+                return secondTeam;
+        }
+    }
+}
